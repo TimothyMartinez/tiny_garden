4849ded5647437c552633ef5271a613ca2d5face
Timothy Martinez <tmrcmartinez@gmail.com>
2019-10-10 16:07:03 -0400

    changes toward a more mobile responsive layout

--- app/javascript/packs/product.js
+++ app/javascript/packs/product.js
@@ -4,8 +4,6 @@ import Plants from '../bundles/Plants/components'
 import FeaturedList from '../bundles/FeaturedProducts/components/FeaturedList'
 import CarePage from '../bundles/CarePage/components/CarePage'
 
-
-
 // This is how react_on_rails can see the HelloWorld in the browser.
 ReactOnRails.register({
   Cart,
