77b5c092b548c8af67cd25d87d34bd4f65ccb002
Timothy Martinez <tmrcmartinez@gmail.com>
2019-12-04 23:19:49 -0500

    syntax and format changes on the commited files

--- app/javascript/bundles/Plants/components/Filter.jsx
+++ app/javascript/bundles/Plants/components/Filter.jsx
@@ -1,24 +1,23 @@
 import React from 'react';
 
 const Filter = ({ filterType, filterParams = [], handleClick }) => (
-  <div className="filter-product-box" >
-    <div className="filter-header">
-      <h4>{filterType}</h4>
-    </div>
+    <div className="filter-product-box" >
+        <div className="filter-header">
+            <h4>{filterType}</h4>
+        </div>
     <div className="filter-list-items">
-      {filterParams.map(({ value, label }) => (
-        <React.Fragment key={label}>
-          <div className="check-alignment">
-            <input type="checkbox" value={value} onClick={() => handleClick(filterType, value)} />
-            <label htmlFor="exampleRadios1">
-              {label}
-            </label>
-          </div>
-        
-        </React.Fragment>
-      ))}
+        {filterParams.map(({ value, label }) => (
+            <React.Fragment key={label}>
+                <div className="check-alignment">
+                    <input type="checkbox" value={value} onClick={() => handleClick(filterType, value)} />
+                    <label htmlFor="exampleRadios1">
+                        {label}
+                    </label>
+                </div>
+            </React.Fragment>
+            ))}
+        </div>
     </div>
-  </div>
 )
 
 export default Filter;
--- app/javascript/bundles/Plants/components/index.jsx
+++ app/javascript/bundles/Plants/components/index.jsx
@@ -5,50 +5,50 @@ import Filter from './Filter'
 import { filters } from './utils'
 
 class Plants extends Component {
-  state = { 
-    plants: [],
-    filters: { size: [], light: [], maintenance: [], category: [] }
-  }
-  
-  componentDidMount(){
-    this.fetchPlants()
-  }
+    state = { 
+        plants: [],
+        filters: { size: [], light: [], maintenance: [], category: [] }
+    }
 
-  fetchPlants = () => {
-    const { filters } = this.state;
-    const searchParams = ['size', 'light', 'maintenance', 'category'].reduce((acc, filterType) => {
-      const filter = filters[filterType] || [];
-      if (!filter.length) return acc;
-      const filterParams = `${filterType}=${filter.join(',')}`;
-      return acc ? `${acc}&${filterParams}` : filterParams;
-    }, '');
-    const url = searchParams ? `/plants.json?${searchParams}` : '/plants.json';
-    axios.get(url).then(({ data }) => this.setState({ plants: data.plants }));
-  };
+    componentDidMount(){
+        this.fetchPlants()
+    }
 
-  handleFilter = (type, value) => {
-    const filters = this.state.filters[type]
-    const updatedFilter = filters.includes(value) 
-      ? filters.filter(currentFilter => currentFilter !== value)
-      : [...filters, value]
-    this.setState({ filters: { ...this.state.filters, [type]: updatedFilter }}, this.fetchPlants)
-  }
+    fetchPlants = () => {
+        const { filters } = this.state;
+        const searchParams = ['size', 'light', 'maintenance', 'category'].reduce((acc, filterType) => {
+        const filter = filters[filterType] || [];
+        if (!filter.length) return acc;
+        const filterParams = `${filterType}=${filter.join(',')}`;
+        return acc ? `${acc}&${filterParams}` : filterParams;
+        }, '');
+        const url = searchParams ? `/plants.json?${searchParams}` : '/plants.json';
+        axios.get(url).then(({ data }) => this.setState({ plants: data.plants }));
+    };
 
-  render() {
-    const { plants } = this.state;
-    return (
-      <div className="center-filter-items">
-        <div className="checkbox-layout">
-          {
-            filters.map((filter, i) => <div key={`filter-${i}`}><Filter {...filter} handleClick={this.handleFilter}/></div>)
-          }
-        </div>
-        <div className="plants-list">
-          <PlantList plants={plants}/>
-        </div>
-      </div>
-    )
-  }
+    handleFilter = (type, value) => {
+        const filters = this.state.filters[type]
+        const updatedFilter = filters.includes(value) 
+        ? filters.filter(currentFilter => currentFilter !== value)
+        : [...filters, value]
+        this.setState({ filters: { ...this.state.filters, [type]: updatedFilter }}, this.fetchPlants)
+    }
+
+    render() {
+        const { plants } = this.state;
+        return (
+            <div className="plants-list">
+                <div className="center-filter-items">
+                <div className="checkbox-layout">
+                    {
+                        filters.map((filter, i) => <div key={`filter-${i}`}><Filter {...filter} handleClick={this.handleFilter}/></div>)
+                    }
+                </div>
+                    <PlantList plants={plants}/>
+                </div>
+            </div>
+        )
+    }
 }
 
 export default Plants;
